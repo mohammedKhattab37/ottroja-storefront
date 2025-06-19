46493ee3e69a000a86ff6a4fb04a8493b80f5a16
Amar2Kk <amarabas9733@gmail.com>
2025-06-19 14:02:13 +0300

    feat(next-intl): setup next intl for internationalization support

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,43 @@
+import { routing } from '@/i18n/routing'
+import type { Metadata } from 'next'
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { notFound } from 'next/navigation'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Ottroja Storefront',
+  description: 'Ottroja Storefront',
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  return (
+    <html lang={locale}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}
