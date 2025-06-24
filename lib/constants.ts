f301cd984352a4cd0030837f009a09824d5dbe42
Doomzy <mohamed_abdulhamid5@outlook.com>
2025-06-24 19:03:55 +0300

    feat(categories): implement dynamic categories section with localized content and images

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+
+export const categoriesSlugs= {
+  "cat1": "honey",
+  "cat2": "mountain-honey",
+  "cat3": "honey-mixtures",
+  "cat4": "honey-capsules",
+  "cat5": "sesame-tahini",
+  "cat6": "natural-oils",
+}
