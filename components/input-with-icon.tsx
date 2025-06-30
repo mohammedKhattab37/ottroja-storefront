84527abb2918fa84536c2b2e800e4b323042007e
Doomzy <mohamed_abdulhamid5@outlook.com>
2025-06-30 15:06:57 +0300

    refactor(categories): rename slug to url in CategoriesSection and ProductsSection refactor(input): update InputWithIcon to accept type and iconWrapperClasses props

--- components/input-with-icon.tsx
+++ components/input-with-icon.tsx
@@ -4,29 +4,31 @@ import { Input } from './ui/input'
 
 function InputWithIcon({
   classNames,
+  iconWrapperClasses,
   name,
+  type = 'text',
   placeholder,
   direction = 'ltr',
   icon,
 }: {
   classNames?: string
+  iconWrapperClasses?: string
   name: string
+  type?: string
   placeholder?: string
   direction: string
   icon: ReactNode
 }) {
   return (
-    <div className="hidden items-center md:grid">
-      <div className="relative" dir={direction}>
-        <div className="absolute top-2.5 ms-2 h-4 w-4">{icon}</div>
-        <Input
-          id={name}
-          type="search"
-          placeholder={placeholder}
-          dir={direction}
-          className={cn('ps-9', classNames)}
-        />
-      </div>
+    <div className="relative" dir={direction}>
+      <div className={cn('absolute top-1/4 ms-2 h-fit w-fit', iconWrapperClasses)}>{icon}</div>
+      <Input
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        dir={direction}
+        className={cn('ps-9', classNames)}
+      />
     </div>
   )
 }
