a8cca447e239d2b13296264e62768f0d5718580a
Doomzy <mohamed_abdulhamid5@outlook.com>
2025-07-24 22:15:32 +0300

    Use cart loading, fix and add some new elements from the design

--- app/[locale]/(main)/_components/header/Header.tsx
+++ app/[locale]/(main)/_components/header/Header.tsx
@@ -24,24 +24,27 @@ function Header() {
   const cartT = useTranslations('cart')
   const locale = useLocale()
   const direction = locale == 'ar' ? 'rtl' : 'ltr'
+  const translatedCurrency = locale == 'ar' ? 'جنيه مصري' : 'EGP'
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const totalItems = useCartStore((state) => state.getTotalItems())
+  const totalPrice = useCartStore((state) => state.getSubtotal())
+  const { isLoading } = useCartStore()
 
   return (
-    <div className="mt-8">
+    <div className="mt-8" dir={locale != 'ar' ? 'rtl' : 'ltr'}>
       <div className="container-padding flex flex-row-reverse items-center justify-between rounded-t-sm py-7 md:px-10">
         <div className="flex w-full items-center justify-end gap-4 sm:gap-6 md:mb-0 md:w-fit md:gap-8">
           <div className="hidden items-center lg:grid">
             <InputWithIcon
               name="search"
-              classNames="bg-hero border-input-border text-secondary placeholder:text-secondary w-xl py-3 ps-8"
+              classNames="bg-hero border-input-border text-secondary placeholder:text-secondary w-sm xl:w-xl py-3 ps-8"
               placeholder={t('header.search')}
               direction={direction}
               icon={<Search className="size-5" />}
               iconWrapperClasses="top-1/5"
             />
           </div>
-          <Link href="">
+          <Link href="/">
             <Image
               src="/assets/logo/header-logo.svg"
               alt=""
@@ -51,23 +54,31 @@ function Header() {
             />
           </Link>
         </div>
-        <div className="text-secondary flex w-fit justify-end gap-3 sm:gap-4">
+        <div className="text-secondary flex w-full justify-start gap-3 sm:gap-4">
           <Profile authT={authT} direction={direction} userButtonText={t('header.user-button')} />
+          <div className="flex content-center items-center gap-2.5">
+            <span className="hidden text-xs font-medium md:block" dir={direction}>
+              {totalPrice} {translatedCurrency}
+            </span>
+            <Button
+              variant={'vanilla'}
+              size={'icon'}
+              className="border-secondary relative h-fit rounded-full border-[1.5px] p-2"
+              onClick={() => setIsDrawerOpen(true)}
+            >
+              {(totalItems > 0 || isLoading) && (
+                <span className="bg-success absolute -top-1.5 -right-2 flex h-4 w-4 items-center justify-center rounded-full text-[10px] text-white sm:h-5 sm:w-5 sm:text-xs">
+                  {isLoading ? (
+                    <div className="border-success border-t-input h-3/4 w-3/4 animate-spin rounded-full border-4"></div>
+                  ) : (
+                    totalItems
+                  )}
+                </span>
+              )}
 
-          <Button
-            variant={'vanilla'}
-            size={'icon'}
-            className="border-secondary relative h-fit rounded-full border-[1.5px] p-2"
-            onClick={() => setIsDrawerOpen(true)}
-          >
-            {totalItems > 0 && (
-              <span className="bg-success absolute -top-1.5 -right-2 flex h-4 w-4 items-center justify-center rounded-full text-[10px] text-white sm:h-5 sm:w-5 sm:text-xs">
-                {totalItems}
-              </span>
-            )}
-
-            <ShoppingCart className="size-4" />
-          </Button>
+              <ShoppingCart className="size-4" />
+            </Button>
+          </div>
           <LangSwitch />
         </div>
         <CustomDrawer
@@ -82,7 +93,7 @@ function Header() {
           contentDirection={direction}
           showInBigScreens={true}
         >
-          <Cart t={cartT} />
+          <Cart t={cartT} setIsDrawerOpen={setIsDrawerOpen} />
         </CustomDrawer>
       </div>
       <NavItems direction={direction} />
