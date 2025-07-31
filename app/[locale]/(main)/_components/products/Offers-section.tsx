985a23e5248e03125b3f7144e9f4d99f23a9575e
Doomzy <mohamed_abdulhamid5@outlook.com>
2025-07-31 17:39:56 +0300

    Modify the offers component and product small card to use the new data for discounted variants

--- app/[locale]/(main)/_components/products/Offers-section.tsx
+++ app/[locale]/(main)/_components/products/Offers-section.tsx
@@ -1,11 +1,13 @@
-import BannerButton from '@/components/banner-button'
 import Header from '@/components/header'
 import ProductSmallCard from '@/components/product-small-card'
-import { dummyOfferCard, dummyProduct } from '@/lib/dummy-data'
 import { useLocale, useTranslations } from 'next-intl'
-import Image from 'next/image'
+import { DiscountedVariantResponse } from '../../products/_actions/types'
 
-function OffersSection() {
+function OffersSection({
+  discountedVariants,
+}: {
+  discountedVariants: DiscountedVariantResponse[]
+}) {
   const t = useTranslations('homePage')
   const locale = useLocale()
   const contentDirection = locale == 'ar' ? 'rtl' : 'ltr'
@@ -13,66 +15,10 @@ function OffersSection() {
   return (
     <div className="justify-items-center sm:px-0">
       <Header title={t('sections.offers')} direction={contentDirection} />
-      <div className="mt-20 grid grid-cols-1 gap-5 xl:grid-cols-3">
-        <div className="grid gap-6">
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-        </div>
-        <div
-          className="bg-card border-car border-border relative h-full w-full overflow-hidden rounded-md border-[1px] p-8 pb-72"
-          dir={contentDirection}
-        >
-          <Image
-            className="absolute start-0 end-0 -top-5 z-[-1]"
-            alt="Bee hive background pattern"
-            src={'/assets/bg-bee-hive.webp'}
-            width={600}
-            height={600}
-            quality={85}
-          />
-          <div className="flex justify-center">
-            <Image
-              className="absolute bottom-0"
-              alt={'Hero illustration'}
-              src={'/assets/hero.svg'}
-              width={300}
-              height={300}
-              quality={100}
-            />
-          </div>
-          <div className="text-secondary space-y-6" dir={contentDirection}>
-            <div className="space-y-1">
-              <div className="flex items-center gap-1">
-                <Image
-                  alt="Bee"
-                  src={'/assets/illustrations/bee.svg'}
-                  width={70}
-                  height={70}
-                  quality={100}
-                  style={{ transform: contentDirection == 'ltr' ? 'scaleX(-1)' : undefined }}
-                />
-                <span className="text-2xl font-semibold">{dummyOfferCard.name}</span>
-              </div>
-              <h1 className="text-2xl leading-tight font-bold sm:text-3xl">
-                {dummyOfferCard.title}
-              </h1>
-              <p className="text-secondary/80 max-w-3/4 text-base leading-relaxed">
-                {dummyOfferCard.sub_title}
-              </p>
-            </div>
-            <BannerButton
-              text={dummyOfferCard.button_text}
-              url={dummyOfferCard.button_destination}
-              size="sm"
-            />
-          </div>
-        </div>
-        <div className="grid gap-y-6">
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-          <ProductSmallCard direction={contentDirection} product={dummyProduct} />
-        </div>{' '}
+      <div className="mt-20 grid grid-cols-3 gap-6">
+        {discountedVariants.map((variant) => (
+          <ProductSmallCard key={variant.id} locale={locale} variant={variant} />
+        ))}
       </div>
     </div>
   )
--- components/product-small-card.tsx
+++ components/product-small-card.tsx
@@ -1,43 +1,75 @@
+import { DiscountedVariantResponse } from '@/app/[locale]/(main)/products/_actions/types'
 import { Link } from '@/i18n/navigation'
 import { cn } from '@/lib/utils'
+import { useCartStore } from '@/stores/cart'
 import { ShoppingCart, Star } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
+import toast from 'react-hot-toast'
 import { Button } from './ui/button'
 
-interface ProductData {
-  name: string
-  url: string
-  rating: number
-  image: string
-  old_price: number
-  new_price: number
-  currency: string
-  discount: number
-}
-
-function ProductSmallCard({ direction, product }: { direction: string; product: ProductData }) {
+function ProductSmallCard({
+  locale,
+  variant,
+}: {
+  locale: string
+  variant: DiscountedVariantResponse
+}) {
+  const { addItem, isLoading } = useCartStore()
   const t = useTranslations('homePage')
+  const productT = useTranslations('products')
+
+  const discountAmount = Math.round(
+    ((variant.price - (variant.compare_at_price || 0)) / variant.price) * 100,
+  )
+  const translatedCurrency = locale == 'ar' ? 'جنيه مصري' : 'EGP'
+  const translatedName =
+    locale == 'ar'
+      ? variant.product.name_ar + ' - ' + variant.variant_name_ar
+      : variant.product.name_en + ' - ' + variant.variant_name_en
+
+  const handleAddToCart = async () => {
+    try {
+      if (!variant.inventory || variant.inventory.quantityAvailable <= 0) {
+        toast.error('Not enough in stock')
+        return
+      }
+
+      addItem({
+        id: variant.id,
+        name_ar: variant.product.name_ar,
+        name_en: variant.product.name_en,
+        quantity: 1,
+        slug: variant.product.slug,
+        image: variant.images[0].url,
+        productVariantId: variant.id,
+        productVariant: variant,
+      })
+
+      toast.success(productT('added-to-cart'))
+    } catch (error) {
+      console.error('Failed to add to cart:', error)
+    }
+  }
 
   return (
-    <div
-      className="bg-card border-border text-card-foreground relative grid grid-flow-col gap-x-5 rounded-md border-[1px] px-10 py-3"
-      dir={direction}
-    >
+    <div className="bg-card border-border text-card-foreground relative flex gap-x-5 rounded-md border-[1px] px-10 py-3">
       <span className="bg-discount-badge absolute start-4 top-3 rounded-full px-2 py-1 text-xs font-semibold text-white drop-shadow-xl">
-        {t('badges.discount') + ' ' + product.discount} %
+        {t('badges.discount') + ' ' + discountAmount} %
       </span>
-      <Image
-        alt={product.name}
-        src={product.image}
-        width={120}
-        height={120}
-        quality={90}
-        sizes="120px"
-        style={{ objectFit: 'contain' }}
-        className="self-center"
-      />
-      <div className="flex flex-col justify-between py-3">
+      {variant.images && (
+        <Image
+          alt={translatedName}
+          src={variant.images[0].url}
+          width={50}
+          height={120}
+          quality={90}
+          sizes="120px"
+          style={{ objectFit: 'contain' }}
+          className="h-24 w-24 self-center"
+        />
+      )}
+      <div className="flex w-full flex-col justify-between py-3">
         {/* name & rating */}
         <div>
           <div className="mb-2 flex items-center gap-1">
@@ -47,22 +79,23 @@ function ProductSmallCard({ direction, product }: { direction: string; product: 
                 stroke="currentColor"
                 className={cn(
                   'h-3.5 w-3.5',
-                  i < product.rating
-                    ? 'fill-yellow-400 text-yellow-400'
-                    : 'fill-[#E8E8E8] text-[#E8E8E8]',
+                  i < 3 ? 'fill-yellow-400 text-yellow-400' : 'fill-[#E8E8E8] text-[#E8E8E8]',
                 )}
               />
             ))}
           </div>
-          <Link href={product.url} className="text-sm font-bold">
-            {product.name}
+          <Link
+            href={`/products/${variant.product.slug}?variant_id=${variant.id}`}
+            className="text-sm font-bold"
+          >
+            {translatedName}
           </Link>
         </div>
         {/* prices & cart btn  */}
         <div className="flex w-full justify-between">
           <div>
             <span className="text-muted text-xs line-through">
-              <span className="font-bold">{product.old_price} </span>/ {product.currency}
+              <span className="font-bold">{variant.price} </span>/ {translatedCurrency}
             </span>
             <span className="flex items-center gap-x-1">
               <Image
@@ -72,11 +105,16 @@ function ProductSmallCard({ direction, product }: { direction: string; product: 
                 height={20}
                 quality={100}
               />
-              <span className="font-bold">{product.new_price} </span>/ {product.currency}
+              <span className="font-bold">{variant.compare_at_price} </span>/ {translatedCurrency}
             </span>
           </div>
           <div className="content-end ps-10 pb-1.5">
-            <Button className="border-card-foreground hover:border-primary hover:text-primary text-card-foreground w-14 rounded-3xl border-[1px] bg-transparent hover:bg-transparent">
+            <Button
+              type="button"
+              disabled={isLoading}
+              onClick={handleAddToCart}
+              className="border-card-foreground hover:border-primary hover:text-primary text-card-foreground w-14 rounded-3xl border-[1px] bg-transparent hover:bg-transparent"
+            >
               <ShoppingCart className="size-[18px]" />
             </Button>
           </div>
