2e08f78c0ccdbd6d5231036d5ce0b056504b8806
abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2025-08-01 22:18:14 +0300

    Update WhyChooseUs component with localized content
    - Removed dependency on translation function and replaced it with static Arabic content.
    - Enhanced item descriptions to provide detailed information about the company, vision, mission, and goals.
    - Adjusted layout and styling for better readability and presentation of the new content.

--- app/[locale]/(main)/about-us/_components/why-choose-us.tsx
+++ app/[locale]/(main)/about-us/_components/why-choose-us.tsx
@@ -1,28 +1,49 @@
 import Image from 'next/image'
 
-function WhyChooseUs({ t }: { t: (key: string) => string }) {
+function WhyChooseUs() {
   const itemsList = [
     {
-      title: t('packaging'),
-      description: t('packaging-description'),
+      title: 'من نحن – شركة أترجة',
+      description: (
+        <>
+          <strong>أترجة (Ottroja)</strong> هي شركة مصرية متخصصة في إنتاج وتوزيع <em>المنتجات الطبيعية</em> مثل الأعسال والزيوت الطبيعية والمكملات الغذائية الطبيعية ، تأسست عام <strong>2015 م / 1437 هـ</strong> ، انطلقت رحلتنا من إيمان وثقة بأن ما يخرج من <em>الطبيعة الصافية</em> ، يستحق أن يصل للناس كما هو <strong>نقيًا ، نافعًا</strong> ، دون أي إضافات أو تلاعب.
+          <br /><br />
+          اسم <strong>" أترجة "</strong> : لم يُختَر هذا الاسم عشوائيًا ، بل يحمل في معناه <em>روح الشركة وقيمها</em>.
+        </>
+      ),
       icon: '/assets/illustrations/about-us/pack.svg',
       iconSize: 40,
     },
     {
-      title: t('quality'),
-      description: t('quality-description'),
+      title: 'رؤيتنا',
+      description: (
+        <>
+          أن نكون من <strong>الشركات الرائدة</strong> في مجال <em>المنتجات الطبيعية</em> حول العالم ومرجعًا للمنتجات والمكملات الغذائية الطبيعية ، بمستوى من الجودة يجعلنا <strong>أقرب للبيت</strong> من مجرد علامة تجارية فنحن لا نلعب دورًا تجاريًا فقط ولكن دورًا <em>توعويًا ومعرفيًا</em> أيضًا.
+        </>
+      ),
       icon: '/assets/illustrations/about-us/quality.svg',
       iconSize: 50,
     },
     {
-      title: t('delivery'),
-      description: t('delivery-description'),
+      title: 'رسالتنا',
+      description: (
+        <>
+          توفير <strong>منتجات طبيعية موثوقة</strong> ، تُنتَج وتُعبَّأ وفق <em>أعلى المعايير</em>، لتصل إلى المستهلك <strong>بنقائها الكامل</strong> ، وتُعبّر عن التزامنا بـ<em>الشفافية والجودة ، والصدق</em> – وخلق وعي كامل بأهمية المنتجات والمكملات الطبيعية في <strong>ثقافة المجتمع العربي</strong>.
+        </>
+      ),
       icon: '/assets/illustrations/about-us/delivery.svg',
       iconSize: 50,
     },
     {
-      title: t('support'),
-      description: t('support-description'),
+      title: 'أهدافنا',
+      description: (
+        <ol className="list-decimal list-inside space-y-3">
+          <li>تقديم <em>منتجات طبيعية 100 %</em> بجودة عالية وثابته بشفافية وواقعية.</li>
+          <li>أن تصل <em>المنتجات الطبيعية النقية</em> إلى كل بيت ومنزل عربي.</li>
+          <li>تثقيف المستهلك حول الفرق بين <em>العسل الطبيعي والمغشوش</em> ، والزيت النقي والمُعدَّل ، والمكمل الغذائي الطبيعي والمصنع و تقديم <strong>محتوى مفيد ونافع</strong> حول المنتجات الطبيعية.</li>
+          <li>الحفاظ على علاقة تقوم على <em>الثقة المتبادلة</em> مع كل عميل.</li>
+        </ol>
+      ),
       icon: '/assets/illustrations/about-us/support.svg',
       iconSize: 50,
     },
@@ -30,62 +51,6 @@ function WhyChooseUs({ t }: { t: (key: string) => string }) {
 
   return (
     <div className="max-width-container">
-      <div className="grid place-items-center gap-2 pb-20 text-center">
-        <p className="mb-10 text-2xl font-bold">من نحن – شركة أترجة</p>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          أترجة (Ottroja) هي شركة مصرية متخصصة في إنتاج وتوزيع المنتجات الطبيعية مثل الأعسال والزيوت
-          الطبيعية والمكملات الغذائية الطبيعية ، تأسست عام 2015 م / 1437 هـ ، انطلقت رحلتنا من إيمان
-          وثقة بأن ما يخرج من الطبيعة الصافية ، يستحق أن يصل للناس كما هو نقيًا ، نافعًا ، دون أي
-          إضافات أو تلاعب .
-        </span>
-        <span className="w-full text-justify text-sm leading-9 font-bold md:w-[90%] md:text-right lg:w-2/3">
-          اسم &quot; أترجة &quot; : لم يُختَر هذا الاسم عشوائيًا ، بل يحمل في معناه روح الشركة
-          وقيمها.
-        </span>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          اسم &quot; أترجة &quot; : مستوحًى من حديث النبي ﷺ : &quot;مثل المؤمن الذي يقرأ القرآن مثل
-          الأترجة ، ريحها طيب وطعمها طيب&quot; وهذا هو ما نطمح إليه : أن تكون منتجاتنا طيبة في
-          المظهر، والمحتوى، والأثر.
-        </span>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          ما هي الأترجة : الأترجة هي نوع من الحمضيات ، تُعرف في بعض البلاد باسم
-          &quot;الاترج&quot; أو &quot;الترنج&quot;، وتُشبه الليمون أو اليوسفي في شكلها ، لكنها أكبر
-          حجمًا وأكثر طيبًا في الرائحة ، ذُكرت في كتب الطب النبوي والطب القديم لما لها من فوائد
-          كثيرة ، وكانت تُعد رمزًا للنقاء والبركة.
-        </span>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          لماذا أترجة ؟ كما تجمع الأترجة بين الطيب في الشكل والطعم والراحة ، تسعى شركة
-          &quot;أترجة&quot; لأن تعكس نفس القيم: نقاء المنتج ، صدق المصدر، وعمق الفائدة.
-        </span>
-      </div>
-      <div className="grid place-items-center gap-10 pb-20 text-center">
-        <p className="text-2xl font-bold">رؤيتنا</p>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          أن نكون من الشركات الرائدة في مجال المنتجات الطبيعية حول العالم ومرجعًا للمنتجات والمكملات
-          الغذائية الطبيعية ، بمستوى من الجودة يجعلنا أقرب للبيت من مجرد علامة تجارية فنحن لا نلعب
-          دورًا تجاريًا فقط ولكن دورًا توعويًا ومعرفيًا أيضًا .
-        </span>
-      </div>
-      <div className="grid place-items-center gap-10 pb-20 text-center">
-        <p className="text-2xl font-bold">رسالتنا</p>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          توفير منتجات طبيعية موثوقة ، تُنتَج وتُعبَّأ وفق أعلى المعايير، لتصل إلى المستهلك بنقائها
-          الكامل ، وتُعبّر عن التزامنا بالشفافية والجودة ، والصدق – وخلق وعي كامل بأهمية المنتجات
-          والمكملات الطبيعية في ثقافة المجتمع العربي.
-        </span>
-      </div>
-      <div className="grid place-items-center gap-10 pb-20 text-center">
-        <p className="text-2xl font-bold">أهدافنا</p>
-        <span className="w-full text-justify text-sm leading-9 font-medium md:w-[90%] md:text-right lg:w-2/3">
-          1 - تقديم منتجات طبيعية 100 % بجودة عالية وثابته بشفافية وواقعية.
-          <br />
-          2 - أن تصل المنتجات الطبيعية النقية إلى كل بيت ومنزل عربي.
-          <br />
-          3 - تثقيف المستهلك حول الفرق بين العسل الطبيعي والمغشوش ، والزيت النقي والمُعدَّل ،
-          والمكمل الغذائي الطبيعي والمصنع و تقديم محتوى مفيد ونافع حول المنتجات الطبيعية.
-          <br />4 - الحفاظ على علاقة تقوم على الثقة المتبادلة مع كل عميل.
-        </span>
-      </div>
       <div className="grid grid-cols-1 gap-x-10 gap-y-20 py-10 md:grid-cols-2 lg:gap-x-20">
         {itemsList.map((item) => (
           <div
@@ -96,8 +61,8 @@ function WhyChooseUs({ t }: { t: (key: string) => string }) {
               <Image src={item.icon} alt="" width={item.iconSize} height={item.iconSize} />
             </div>
             <div>
-              <p className="pb-4 text-sm font-bold">{item.title}</p>
-              <p className="text-xs leading-9 font-medium">{item.description}</p>
+              <p className="pb-4 text-base font-bold">{item.title}</p>
+              <div className="text-sm leading-7 font-medium">{item.description}</div>
             </div>
           </div>
         ))}
